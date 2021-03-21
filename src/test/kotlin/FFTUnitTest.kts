514a0ccf25f936ac883498e4876c0f01b80c4aab
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 21:07:21 +0800

    Create FFTUnitTest.kts

---
+++ src/test/kotlin/FFTUnitTest.kts
@@ -0,0 +1,7 @@
+import kotlin.math.sqrt
+
+val arr = arrayOf(Complex(0.0), Complex(0.5), Complex(sqrt(3.0)/2), Complex(1.0), Complex(sqrt(3.0)/2), Complex(0.5)) // y = sin(pi*t/3)
+
+val fft = FFT.fft(arr)
+
+println(fft)
