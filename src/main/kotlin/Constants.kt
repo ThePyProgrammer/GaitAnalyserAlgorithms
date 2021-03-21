45994a7986f9be95b7e98ff7e47548f7703ffe8d
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 15:34:35 +0800

    Update Constants.kt

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -6,25 +6,25 @@ import kotlin.math.roundToInt
 
 @ExperimentalNumkt
 object Constants {
-    val SR = 64F
-    val stepSize = 1F
-    val offDelay = 2F
-    val onDelay = 2F
+    val SR = 64.0
+    val stepSize = 1.0
+    val offDelay = 2.0
+    val onDelay = 2.0
 
-    val NFFT = 256F
-    val windowLength = 256F
-    val locoBand = array(arrayOf<Float>(0.5F, 3F))
-    val freezeBand = array(arrayOf<Float>(3F, 8F))
+    val NFFT = 256.0
+    val windowLength = 256.0
+    val locoBand = array(arrayOf(0.5, 3.0))
+    val freezeBand = array(arrayOf(3.0, 8.0))
 
-    val f_res: Float = SR / NFFT
-    val f_nr_LBs: Int? = locoBand[0, 0].scalar?.div(f_res)?.let { it.roundToInt() }
-    val f_nr_LBe: Int? = locoBand[0, 1].scalar?.div(f_res)?.let { it.roundToInt() }
-    val f_nr_FBs: Int? = freezeBand[0, 0].scalar?.div(f_res)?.let { it.roundToInt() }
-    val f_nr_FBe: Int? = freezeBand[0, 1].scalar?.div(f_res)?.let { it.roundToInt() }
+    val f_res = SR / NFFT
+    val f_nr_LBs: Int = locoBand[0, 0].scalar?.div(f_res)?.let { it.roundToInt() }!!
+    val f_nr_LBe: Int = locoBand[0, 1].scalar?.div(f_res)?.let { it.roundToInt() }!!
+    val f_nr_FBs: Int = freezeBand[0, 0].scalar?.div(f_res)?.let { it.roundToInt() }!!
+    val f_nr_FBe: Int = freezeBand[0, 1].scalar?.div(f_res)?.let { it.roundToInt() }!!
 
     val d = NFFT / 2
 
-    val freezeTH = 1.5F
-    val powerTH = 2F.pow(11.5F)
+    val freezeTH = 1.5
+    val powerTH = 2.0.pow(11.5)
 
 }
