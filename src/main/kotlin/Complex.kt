b0e66b6bdc04690ef7a4dce5c1360d4fd8afbd0f
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 15:34:32 +0800

    Update Complex.kt

--- src/main/kotlin/Complex.kt
+++ src/main/kotlin/Complex.kt
@@ -1,14 +1,42 @@
 import kotlin.math.*
 
-class Complex(val re: Double, val im: Double) {
+class Complex(val re: Double, val im: Double = 0.0) {
     infix operator fun plus(x: Complex) = Complex(re + x.re, im + x.im)
+    infix operator fun plus(x: Double) = Complex(re + x, im)
+    infix operator fun plus(x: Int) = Complex(re + x, im)
+
     infix operator fun minus(x: Complex) = Complex(re - x.re, im - x.im)
+    infix operator fun minus(x: Double) = Complex(re - x, im)
+    infix operator fun minus(x: Int) = Complex(re - x, im)
+
     infix operator fun times(x: Double) = Complex(re * x, im * x)
+    infix operator fun times(x: Int) = Complex(re * x, im * x)
     infix operator fun times(x: Complex) = Complex(re * x.re - im * x.im, re * x.im + im * x.re)
+
     infix operator fun div(x: Double) = Complex(re / x, im / x)
-    val exp: Complex by lazy { Complex(cos(im), kotlin.math.sin(im)) * (kotlin.math.cosh(re) + kotlin.math.sinh(
+
+    val exp: Complex by lazy { Complex(cos(im), sin(im)) * (cosh(re) + sinh(
         re
     )) }
+
+    val mag = sqrt(re*re + im*im)
+    val unit = if(mag != 0.0) Complex(re/mag, im/mag) else Complex(0.0, 0.0)
+    val alpha = abs(asin(unit.im))
+    val arg = {
+        if(im == 0.0 && re == 0.0) 0
+        else if(im == 0.0 && re > 0) 0
+        else if(im == 0.0 && re < 0) PI
+        else if(re == 0.0 && im > 0) PI/2
+        else if(re == 0.0 && im < 0) - PI/2
+        else if(re > 0 && im > 0) alpha
+        else if(re > 0 && im < 0) -alpha
+        else if(re < 0 && im > 0) PI - alpha
+        else alpha - PI
+    }
+
+    val conj = Complex(re, -im)
+
+
 
     override fun toString() = when {
         b == "0.000" -> a
@@ -18,5 +46,6 @@ class Complex(val re: Double, val im: Double) {
     }
 
     private val a = "%1.3f".format(re)
-    private val b = "%1.3f".format(kotlin.math.abs(im))
+    private val b = "%1.3f".format(abs(im))
 }
+
