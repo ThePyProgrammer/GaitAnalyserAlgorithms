d1292dffceea51ec64460b79a918c38b349228bf
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 21:30:22 +0800

    Update FFTUnitTest.kts

--- src/test/kotlin/FFTUnitTest.kts
+++ src/test/kotlin/FFTUnitTest.kts
@@ -4,8 +4,16 @@ import kotlin.math.sqrt
 
 val arr = arrayOf(Complex(0.0), Complex(0.5), Complex(sqrt(3.0)/2), Complex(1.0), Complex(sqrt(3.0)/2), Complex(0.5), Complex(.0), Complex(-0.5)) // y = sin(pi*t/3)
 
-println(arr.size)
-
 val fft = FFT.fft(arr)
 
 println(fft.joinToString(prefix="{", postfix="}"))
+
+// Answer for the code:
+// {3.232, -1.927 + 1.927i, 0.500i, 0.195 + 0.195i, 0.232, 0.195 - 0.195i, -0.000 - 0.500i, -1.927 - 1.927i}
+
+
+// Answer according to Python:
+// array([ 3.23205081-0.j        , -1.92668558-1.92668558j,
+//        0.        -0.5j       ,  0.19463477-0.19463477j,
+//        0.23205081-0.j        ,  0.19463477+0.19463477j,
+//        0.        +0.5j       , -1.92668558+1.92668558j])
