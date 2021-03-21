716e970f5d5a58005149304eed7b49863276176d
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 21:15:25 +0800

    Update FFTUnitTest.kts

--- src/test/kotlin/FFTUnitTest.kts
+++ src/test/kotlin/FFTUnitTest.kts
@@ -1,6 +1,10 @@
+import com.thepyprogrammer.gaitanalyzeralgos.Complex
+import com.thepyprogrammer.gaitanalyzeralgos.FFT
 import kotlin.math.sqrt
 
 val arr = arrayOf(Complex(0.0), Complex(0.5), Complex(sqrt(3.0)/2), Complex(1.0), Complex(sqrt(3.0)/2), Complex(0.5)) // y = sin(pi*t/3)
+
+println(arr.size)
 
 val fft = FFT.fft(arr)
 
