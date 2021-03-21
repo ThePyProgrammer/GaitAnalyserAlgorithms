557e20e475aebe100063fae5a214016f2845ab55
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-21 21:13:01 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,9 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://dl.bintray.com/kotlin/kotlin-datascience")
 }
 
 dependencies {
-    implementation("org.jetbrains:kotlin-numpy:0.1.5")
     testImplementation(kotlin("test-junit"))
     testImplementation(kotlin("script-runtime"))
 }
